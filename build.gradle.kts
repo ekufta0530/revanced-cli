c23946f48d185abce3d0c2ec81a225f22c4f0e8b
Eric Kufta <eric.kufta@semgrep.com>
2024-08-09 13:28:06 -0700

    Updated build.gradle.kts for lockfile generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,21 +11,29 @@ plugins {
 group = "app.revanced"
 
 application {
-    mainClass = "app.revanced.cli.command.MainCommandKt"
+    mainClass.set("app.revanced.cli.command.MainCommandKt")
 }
 
 repositories {
     mavenCentral()
     mavenLocal()
     google()
+    // Uncomment and properly configure the repository if needed
+    /*
     maven {
-        // A repository must be speficied for some reason. "registry" is a dummy.
+        // A repository must be specified for some reason. "registry" is a dummy.
         url = uri("https://maven.pkg.github.com/revanced/registry")
         credentials {
             username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
             password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
         }
     }
+    */
+}
+
+// Activate dependency locking for all relevant configurations
+configurations.all {
+    resolutionStrategy.activateDependencyLocking()
 }
 
 dependencies {
@@ -38,8 +46,8 @@ dependencies {
 }
 
 kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_11)
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
@@ -72,10 +80,6 @@ tasks {
     }
 }
 
-// Needed by gradle-semantic-release-plugin.
-// Tracking: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
-
-// The maven-publish is also necessary to make the signing plugin work.
 publishing {
     repositories {
         mavenLocal()
@@ -90,6 +94,5 @@ publishing {
 
 signing {
     useGpgCmd()
-
     sign(publishing.publications["revanced-cli-publication"])
 }
